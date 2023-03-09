ec45eebc00dc33e300b8fe9cf4710e7787a48232
nayan <nayansharma1010@gmail.com>
2023-03-09 12:04:55 +0530

    feat; application guards and hosting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get,post,web::{self, Data},App,HttpResponse,HttpServer,Responder};
+use actix_web::{get,post,web::{self, Data, ServiceConfig},App,HttpResponse,HttpServer,Responder, guard};
 use std::sync::Mutex;
 
 struct AppState {
@@ -31,19 +31,48 @@ async fn index2(data: web::Data<AppStateWithCounter>) -> String{
     format!("Request number:{counter}")
 }
 
+fn scoped_config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/test")
+            .route(web::get().to(|| async {HttpResponse::Ok().body("test")}))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)), 
+    );
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/app")
+            .route(web::get().to(|| async {HttpResponse::Ok().body("app")}))
+            .route(web::head().to(HttpResponse::MethodNotAllowed)), 
+    );
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
-    let counter: Data<AppStateWithCounter> = web::Data::new(AppStateWithCounter {
-        counter: Mutex::new(0),
-    });
+    // let counter: Data<AppStateWithCounter> = web::Data::new(AppStateWithCounter {
+    //     counter: Mutex::new(0),
+    // });
 
-    HttpServer::new(move || {
-       App::new()
-            .app_data(counter.clone())
-            .route("/",web::get().to(index2))
-    })
-    .bind(("127.0.0.1",8000))?
-    .run()
-    .await
+    // HttpServer::new(move || {
+    //    App::new()
+    //         .app_data(counter.clone())
+    //         .route("/",web::get().to(index2))
+    // })
+    // .bind(("127.0.0.1",8000))?
+    // .run()
+    // .await
+
+   HttpServer::new(|| {
+    App::new()
+        .configure(config)
+        .service(web::scope("/api").configure(scoped_config))
+        .route(
+            "/",
+            web::get().to(|| async {HttpResponse::Ok().body("/")}),         
+        )
+   })
+   .bind(("127.0.0.1",8080))?
+   .run()
+   .await
 }
